27bdd84de90033c8c7185a2f258880153f9a233d
welingtonsoftware <dpepsloco@gmail.com>
2024-02-08 17:13:35 -0300

    feat: create check vote

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,9 +2,10 @@ import z from "zod";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "crypto";
 import { prisma } from "../../lib/prisma";
+import { serialize } from "v8";
 
 export async function voteOnPoll(app: FastifyInstance) {
-  app.post('/polls/:pollId/votes', async (request, reply) => {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
     try {
       const voteOnPollBody = z.object({
         pollOptionId: z.string().uuid(),
@@ -19,11 +20,36 @@ export async function voteOnPoll(app: FastifyInstance) {
 
       let { sessionId } = request.cookies;
 
+      if (sessionId) {
+        const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+          where: {
+            sessionId_pollId: {
+              sessionId,
+              pollId,
+            },
+          },
+        });
+        if (
+          userPreviousVoteOnPoll &&
+          userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+        ) {
+          await prisma.vote.delete({
+            where: {
+              id: userPreviousVoteOnPoll.id,
+            },
+          });
+        } else if (userPreviousVoteOnPoll) {
+          return reply
+            .status(400)
+            .send({ message: "You alredy voted on this poll." });
+        }
+      }
+
       if (!sessionId) {
         sessionId = randomUUID();
 
-        reply.setCookie('sessionId', sessionId, {
-          path: '/',
+        reply.setCookie("sessionId", sessionId, {
+          path: "/",
           maxAge: 60 * 60 * 24 * 30,
           signed: true,
           httpOnly: true,
@@ -35,9 +61,9 @@ export async function voteOnPoll(app: FastifyInstance) {
           sessionId,
           pollId,
           pollOptionId,
-        }
-      })
-      
+        },
+      });
+
       return reply.status(201).send({ sessionId });
     } catch (error) {
       console.error(error);
